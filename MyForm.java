7dca6f43dc514144b5ac429e39252ae7fbba0324
Wissem BEN AICHA <wissem.ben-aicha@proxym-it.com>
2021-12-21 20:17:20 +0100

    creation of the form layout

--- MyForm.java
+++ MyForm.java
@@ -1,4 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyForm extends JFrame {
+    JLabel nomL=new JLabel("Nom");
+    JLabel prenomL=new JLabel("Prenom");
+    JLabel genreL=new JLabel("genre");
+    JTextField nomI = new JTextField();
+    JTextField prenomI = new JTextField();
+    JButton valider=new JButton("Valider");
+    JButton annuler=new JButton("Annuler");
+    JRadioButton hommeI = new JRadioButton("Homme");
+    JRadioButton femmeI = new JRadioButton("Femme");
+    public MyForm() {
+        super("Exam");
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+        c.add(nomL);
+        c.add(nomI);
+        c.add(prenomL);
+        c.add(prenomI);
+        c.add(genreL);
+        c.add(hommeI);
+        c.add(femmeI);
+        c.add(valider);
+        c.add(annuler);
+        setSize(500,200);
+        setLocationRelativeTo(getParent());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        show();
+    }
+    public static void main (String[] args)
+    {
+        new MyForm();
+    }
 }
