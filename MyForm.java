6f1307929b8886593bac44ad4ef7d1117b724ffc
Wissem BEN AICHA <wissem.ben-aicha@proxym-it.com>
2021-12-22 10:51:50 +0100

    addming list and fixing form layout

--- MyForm.java
+++ MyForm.java
@@ -3,10 +3,13 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MyForm extends JFrame {
+    //les libelles
     JLabel formulaire = new JLabel("Exam Tp");
     JLabel nomL=new JLabel("Nom");
     JLabel genreL=new JLabel("genre");
+    // les Inputs
     JTextField nomI = new JTextField();
+    // les Buttons
     JButton valider=new JButton("Ajouter");
     JButton annuler=new JButton("Annuler");
     JRadioButton hommeI = new JRadioButton("Homme");
@@ -20,19 +23,29 @@ public class MyForm extends JFrame {
         hommeI.setSelected(true);
         bg.add(hommeI);
         bg.add(femmeI);
+        // Panel Formulaire
         JPanel panelFormulaire=new JPanel();
-        panelFormulaire.setLayout(new GridLayout(0,1));
-        Container c = getContentPane();
-        panelFormulaire.add(formulaire);
+        panelFormulaire.setLayout(new GridLayout(5,1));
         panelFormulaire.add(nomL);
         panelFormulaire.add(nomI);
         panelFormulaire.add(genreL);
         panelFormulaire.add(hommeI);
         panelFormulaire.add(femmeI);
-        panelFormulaire.add(valider);
-        panelFormulaire.add(annuler);
-        c.add(panelFormulaire);
-        setSize(500,200);
+        // Panel Buttons
+        JPanel panelBouttons=new JPanel();
+        panelBouttons.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        panelBouttons.add(valider);
+        panelBouttons.add(annuler);
+        // Panel Assemblages
+        JPanel panelAssemblage=new JPanel();
+        panelAssemblage.setLayout(new BorderLayout());
+        panelAssemblage.add("Center",panelFormulaire);
+        panelAssemblage.add("South",panelBouttons);
+        Container c = getContentPane();
+        c.setLayout(new BorderLayout());
+        c.add("North",formulaire);
+        c.add("Center",panelAssemblage);
+        setSize(200,400);
         setLocationRelativeTo(getParent());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         show();
--- MyList.java
+++ MyList.java
@@ -1,2 +1,28 @@
-public class MyList {
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class MyList extends JFrame {
+    String entete[] = {"id","Nom","Genre"};
+    DefaultTableModel model = new DefaultTableModel(entete,3);
+    JTable table = new JTable(model);
+    JScrollPane sp = new JScrollPane();
+    public MyList() {
+        super("MyList");
+        sp.getViewport().add(table);
+        sp.setPreferredSize(new Dimension(600,200));
+        JPanel panelTable = new JPanel();
+        panelTable.add(sp);
+        Container c=getContentPane();
+        c.setLayout(new BorderLayout());
+        c.add("Center",panelTable);
+        setSize(600,400);
+        setLocationRelativeTo(getParent());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        show();
+
+    }
+    public static void main (String[] args) {
+        new MyList();
+    }
 }
